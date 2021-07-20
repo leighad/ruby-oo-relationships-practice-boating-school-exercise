3ff6a9d114a64242d8ef8e06170dedebc9bc4d61
Leigha De La Rosa <cdversiontwo@gmail.com>
2021-07-20 14:42:05 -0700

    add @@all

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,8 +2,15 @@ class Student
 
     attr_accessor :first_name
 
+    @@all = []
+
     def initialize(first_name)
         @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
     end
 
 end
